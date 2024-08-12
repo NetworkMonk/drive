622e100f767fc8d2bb4856c1c8542d1cb1451740
NetworkMonk <jamesplant@gmail.com>
2024-08-12 23:36:09 +0100

    Added optional smoothing of game camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform target;
+    public float smooth = 2.0f;
+    public bool smoothing = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(target.position.x - 3.0f, target.position.y + 5.0f, target.position.z -5.0f);
+        if (smoothing)
+        {
+            transform.position = Vector3.Lerp(transform.position, new Vector3(target.position.x - 3.0f, target.position.y + 5.0f, target.position.z - 5.0f), Time.deltaTime * smooth);
+        }
+        else
+        {
+            transform.position = new Vector3(target.position.x - 3.0f, target.position.y + 5.0f, target.position.z - 5.0f);
+        }
     }
 }
