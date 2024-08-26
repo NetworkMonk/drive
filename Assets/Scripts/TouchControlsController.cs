503e0e05ced395f15a63cb445e811ea6ebe23b72
NetworkMonk <jamesplant@gmail.com>
2024-08-26 21:51:06 +0100

    Resolved issues with text not rendering on iphone, this should be v1 complete

--- Assets/Scripts/TouchControlsController.cs
+++ Assets/Scripts/TouchControlsController.cs
@@ -1,25 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.InteropServices;
 
 public class TouchControlsController : MonoBehaviour
 {
+    [DllImport("__Internal")]
+    private static extern bool IsMobileDevice();
+
     // Start is called before the first frame update
     void Start()
     {
-        if (Input.touchSupported)
+        if (Application.platform == RuntimePlatform.WebGLPlayer)
         {
-            gameObject.SetActive(true);
+            if (IsMobileDevice())
+            {
+                EnableTouchControls();
+            }
+            else
+            {
+                DisableTouchControls();
+            }
         }
         else
         {
-            gameObject.SetActive(false);
+            gameObject.SetActive(Input.touchSupported);
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    void EnableTouchControls()
     {
-        
+        gameObject.SetActive(true);
     }
+
+    void DisableTouchControls()
+    {
+        gameObject.SetActive(false);
+    }
+
 }
