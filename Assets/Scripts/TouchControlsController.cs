f51a6a796156090ce9e4f679eddfaeba1d3cf4e4
NetworkMonk <jamesplant@gmail.com>
2024-08-26 18:29:54 +0100

    Conditional touch controls

--- Assets/Scripts/TouchControlsController.cs
+++ Assets/Scripts/TouchControlsController.cs
@@ -7,7 +7,14 @@ public class TouchControlsController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (Input.touchSupported)
+        {
+            gameObject.SetActive(true);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
     }
 
     // Update is called once per frame
